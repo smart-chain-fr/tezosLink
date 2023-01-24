bce40474a1e56d396a1636f7c063e6e560511d27
Elise <elise.hautefaye@smart-chain.fr>
2023-01-24 17:35:56 +0100

    #smart209 - [WIP] - add classnames package and global scss

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import {Button} from "@/Components/Elements/Button"
-import { Burger } from "@/Components/Elements/Burger"
+import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -15,10 +11,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Button text="coucou"/>
-        <Burger/>
-      </main>
     </>
   )
 }
