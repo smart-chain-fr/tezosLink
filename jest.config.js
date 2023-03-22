14be5f4f72ee30afe756bea33bef88ea497ef6d0
Loïs Mansot <26844641+devfull@users.noreply.github.com>
2023-03-22 16:12:35 +0100

    configure jest with name mapper and ignore dist

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  rootDir: './src/',
+  moduleNameMapper: {
+    "@Api/(.*)": "<rootDir>/api/$1",
+    "@RpcGateway/(.*)": "<rootDir>/rpc-gateway/$1",
+    "@Services/(.*)": "<rootDir>/common/services/$1",
+    "@Repositories/(.*)": "<rootDir>/common/repositories/$1",
+    "@Entries/(.*)": "<rootDir>/entries/$1",
+    "@Common/(.*)": "<rootDir>/common/$1",
+    "@Config/(.*)": "<rootDir>/common/config/$1",
+    "@Entities/(.*)": "<rootDir>/common/ressources/$1",
+    "@System/(.*)": "<rootDir>/common/system/$1",
+    "@ControllerPattern/(.*)": "<rootDir>/common/system/controller-pattern/$1",
+  },
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
