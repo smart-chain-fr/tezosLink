41e826987e0d124f730a3206aa572b0ae9fcee7e
Loïs Mansot <26844641+devfull@users.noreply.github.com>
2023-03-23 12:06:12 +0100

    setup a database connection in tests entry point

--- src/entries/Test.spec.ts
+++ src/entries/Test.spec.ts
@@ -1,5 +1,15 @@
+import "module-alias/register";
+import "reflect-metadata";
+
+import { Container } from "typedi";
+import TezosLink from "@Common/databases/TezosLink";
+
 import { describe } from "@jest/globals";
 import Tests from "@Tests/index";
+
+beforeAll(async () => {
+	await Container.get(TezosLink).connect();
+});
 
 (async () => {
 	describe("Run all tests", function () {
