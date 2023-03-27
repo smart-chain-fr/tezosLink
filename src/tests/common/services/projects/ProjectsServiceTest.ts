d7b787727aa6bcd9547a7837cd47f45b31315a09
Loïs Mansot <26844641+devfull@users.noreply.github.com>
2023-03-27 18:03:23 +0200

    add some test cases for ProjectsRepository

---
+++ src/tests/common/services/projects/ProjectsServiceTest.ts
@@ -0,0 +1,54 @@
+import { Container } from "typedi";
+import { beforeAll, describe, it } from "@jest/globals";
+
+import { v4 as uuidv4 } from "uuid";
+
+import ObjectHydrate from "@Common/helpers/ObjectHydrate"
+import ProjectEntity from "@Entities/projects/ProjectEntity"
+import ProjectsService from "@Services/project/ProjectsService";
+
+export default () => {
+	describe("ProjectsRepository", () => {
+		let projectsService: ProjectsService;
+
+		const projectEntity =
+			ObjectHydrate.hydrate(new ProjectEntity(),
+				{ title: "title", network: "network" }
+			);
+
+		beforeAll(async () => {
+			projectsService = await Container.get(ProjectsService);
+		});
+
+		describe("🗹 Validity tests", () => {
+			it("cannot delete entities without a valid uuid", async () => {
+				expect(
+					projectsService.delete(projectEntity)
+				).rejects.toBeInstanceOf(Error);
+			});
+
+			it("cannot delete entities without an existing uuid", async () => {
+				const projectEntityWithUUID =
+					ObjectHydrate.hydrate(new ProjectEntity(),
+						{ title: "title", network: "network", uuid: uuidv4() }
+					);
+				expect(
+					projectsService.delete(projectEntityWithUUID)
+				).rejects.toBeInstanceOf(Error);
+			});
+
+			it("can create entities", async () => {
+				const createdEntity = await projectsService.create(projectEntity);
+				expect(createdEntity).toBeDefined();
+				projectsService.delete(createdEntity);
+			});
+		});
+
+		describe("⤞ Postcondition tests", () => {
+			it("can delete newly created entities", async () => {
+				const createdEntity = await projectsService.create(projectEntity);
+				expect(projectsService.delete(createdEntity)).resolves.toBeUndefined();
+			});
+		});
+	});
+};
--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,11 +1,13 @@
 import ObjectHydrateTest from "@Tests/common/helpers/ObjectHydrateTest";
 import ProjectEntityTest from "@Tests/common/ressources/projects/ProjectEntityTest";
 import ProjectsRepositoryTest from "@Tests/common/repositories/projects/ProjectsRepositoryTest";
+import ProjectsServiceTest from "@Tests/common/services/projects/ProjectsServiceTest";
 import VariablesTest from "@Tests/common/config/variables/VariablesTest";
 
 export default
 	[ ObjectHydrateTest
 	, ProjectEntityTest
 	, ProjectsRepositoryTest
+	, ProjectsServiceTest
 	, VariablesTest
 	];
