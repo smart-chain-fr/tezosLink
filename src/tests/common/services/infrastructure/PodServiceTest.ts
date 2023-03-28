3255ce5f08cda63a547cf2741faf18ee58e178d2
Loïs Mansot <26844641+devfull@users.noreply.github.com>
2023-03-28 14:05:14 +0200

    add some test cases for PodService

--- src/tests/common/services/infrastructure/PodServiceTest.ts
+++ src/tests/common/services/infrastructure/PodServiceTest.ts
@@ -1,6 +1,8 @@
 import { Container } from "typedi";
 import { beforeAll, describe, it } from "@jest/globals";
 
+import { v4 as uuidv4 } from "uuid";
+
 import ObjectHydrate from "@Common/helpers/ObjectHydrate"
 import PodEntity from "@Entities/infrastructure/PodEntity"
 import PodService from "@Services/infrastructure/PodService";
@@ -23,6 +25,34 @@ export default () => {
 				await expect(
 					podService.delete(podEntity)
 				).rejects.toBeInstanceOf(Error);
+			});
+
+			it("can create entities", async () => {
+				const newEntity =
+					ObjectHydrate.hydrate(new PodEntity(),
+						{ name: uuidv4(), phase: "phase", type: "type" }
+					);
+				const createdEntity = await podService.savePod(newEntity);
+				expect(createdEntity).toBeDefined();
+				await podService.delete(createdEntity);
+			});
+		});
+
+		describe("⤞ Postcondition tests", () => {
+			it("updates only the phase property on existing entities name", async () => {
+				const newEntity =
+					ObjectHydrate.hydrate(new PodEntity(),
+						{ name: uuidv4(), phase: "phase", type: "type" }
+					);
+				const updatedEntity =
+					ObjectHydrate.hydrate(new PodEntity(),
+						{ ...newEntity, phase: "other_phase", type: "other_type" }
+					)
+				await podService.savePod(newEntity);
+				await expect(podService.savePod(updatedEntity)).resolves.toMatchObject(
+					{ ...newEntity, phase: updatedEntity.phase }
+				);
+				await podService.delete(newEntity);
 			});
 		});
 	});
